30449148f8e47f2567c3f755b63aaf21f66e3666
vogiahuy257 <vogiahuy257@gmail.com>
2024-08-25 15:36:00 +0700

    25-08-2024 update layout v1

--- src/components/Certs.jsx
+++ src/components/Certs.jsx
@@ -38,19 +38,16 @@ function Certs() {
           </div>
         </div>
       ) : (
-        <div className="cert-groups">
+        <div className="cert-groups ">
           {Object.keys(groupedCertificates).map(issuer => (
             <div key={issuer} className="cert-group">
               <h2 className="cert-group-title">{groupTitles[issuer]}</h2>
               <div className="cert-list">
                 {groupedCertificates[issuer].map(cert => (
-                  <div 
-                    key={cert.id} 
-                    className="cert-item" 
-                    onClick={() => handleCertClick(cert.id)}
-                  >
+                  <div key={cert.id} className="cert-item" onClick={() => handleCertClick(cert.id)}>
                     <img className="cert-image" src={cert.image} alt={cert.title} />
-                    <h2>{cert.title}</h2>
+
+                    <h2 className='cert-text'>{cert.title}</h2>
                   </div>
                 ))}
               </div>
