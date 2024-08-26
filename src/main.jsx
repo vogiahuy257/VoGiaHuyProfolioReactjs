41d61213f909d638b8b50631049bf8e1a21e4949
vogiahuy257 <vogiahuy257@gmail.com>
2024-08-26 12:58:40 +0700

    26-08-2024 move css to asset css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './components/css/style.css';
+import './assets/css/style.css';
 
 
 
